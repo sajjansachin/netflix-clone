611cfd6d1754c6731f22b98c11cd3133cac15789
Sachin Sajjan <sachinsajjan54@gmail.com>
2022-06-19 08:41:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Row from "./components/Row";
+import request from "./request";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row title="NETFLIX_ORIGINAL" fetchUrl={request.fetchNetflixOriginals} />
+      <Row title="TRENDING" fetchUrl={request.fetchTrending} />
     </div>
   );
 }
